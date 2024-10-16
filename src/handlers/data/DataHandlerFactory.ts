7071500bd6edcdf7210ed2e881040490c432a042
Yente Paternotte <yente.paternotte@hotmail.com>
2024-10-16 20:53:49 +0200

    feat: added csv handler

--- src/handlers/data/DataHandlerFactory.ts
+++ src/handlers/data/DataHandlerFactory.ts
@@ -1,11 +1,13 @@
 import { JsonDataHandler } from "./JsonDataHandler.js";
 import { IDataHandler } from "../../types/IDataHandler.js";
+import { CsvDataHandler } from "./CsvDataHandler.js";
 
 export default class DataHandlerFactory {
     private handlers: { [key: string]: IDataHandler } = {};
 
     constructor() {
         this.handlers['json'] = new JsonDataHandler();
+        this.handlers['csv'] = new CsvDataHandler();
     }
 
     public getHandler(type: string): IDataHandler {
